0349250e5c4888d8d59fa9369a0fdd86f03baa4d
Jovan Orozco <orozcopenn@gmail.com>
2013-07-15 05:54:22 +0000

    changed port

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var buf= new Buffer(fs.readFileSync('index.html'));
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
